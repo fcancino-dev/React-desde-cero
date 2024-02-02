4d9dffbadcb8c1e0d03601b3858a9dd1a2a069f4
fcancino-dev <franco.cancino@unapiquitos.edu.pe>
2024-02-02 15:42:20 -0500

    comenzando con react

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+export function App() {
+  return (
+    <article>
+        <header>
+            <img src="https://unavatar.io/dribbble/omidnikrah" alt="un avatar cualquiera" />
+            <div>
+                <strong>Migueal Angel Duran</strong>
+                <span>@miguel</span>
+            </div>
+        </header>
+        <aside>
+            <button>
+                seguir
+            </button>
+        </aside>
+    </article>
+  );
+}
